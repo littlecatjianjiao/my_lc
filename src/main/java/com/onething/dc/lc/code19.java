d42e84dd98b613908f2d1e3926aa4c142c388184
jiangchunyang <jiangchunyang@onething.net>
2023-06-21 16:15:40 +0800

    [add]change

--- src/main/java/com/onething/dc/lc/code19.java
+++ src/main/java/com/onething/dc/lc/code19.java
@@ -1,16 +1,27 @@
 package com.onething.dc.lc;
 
+import com.sun.org.apache.xpath.internal.WhitespaceStrippingElementMatcher;
+
 /**
- * @ClassPath: com.onething.dc.lc
- * @Description:
- * @Author: jiangchunyang
- * @Date: 2023/5/5
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 示例 2：
+ * <p>
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ * 提示：
  */
 public class code19 {
 
-    public ListNode removeRNthNode(ListNode head, int n) {
+    public ListNode test(ListNode head, int n) {
         ListNode pre = new ListNode(0);
-        ListNode start = pre, end = pre;
+        pre.next = head;
+        ListNode start = pre,end = pre;
         while (n != 0) {
             start = start.next;
             n--;
@@ -20,10 +31,12 @@ public class code19 {
             end = end.next;
         }
         end.next = end.next.next;
-
         return pre.next;
     }
 
+    //1 - 2 - 3 - 4 - 5     n = 5  =>  2 - 3 - 4 - 5
+    //1 - 2 - 3 - 4 - 5     n = 2  =>  1 - 2 - 3 - 5
+    //1 - 2 - 3 - 4 - 5     n = 3  =>  1 - 2 - 4 - 5
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode pre = new ListNode(0);
         pre.next = head;
